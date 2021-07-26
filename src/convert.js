9a36ad1ee05aea0e14066374c6588f523c8d0175
Ekaterina Ivanova <eivanova@cs-cart.com>
2021-07-26 16:57:30 +0400

    convert.js after review

--- src/convert.js
+++ src/convert.js
@@ -1,20 +1,7 @@
-export default (tree) => {
-  const getRecursiveValue = (node) => {
-    if (Array.isArray(node) && !node.length) {
-      return {};
-    }
-    if (!Array.isArray(node)) {
-      return node;
-    }
+const convert = (node) => node.reduce((resObj, item) => {
+  const [key, value] = item;
+  const newValue = Array.isArray(value) ? convert(value) : value;
+  return { ...resObj, [key]: newValue };
+}, {});
 
-    const result = node.reduce((resObj, item) => {
-      const [key, value] = item;
-      resObj[key] = getRecursiveValue(value);
-      return { ...resObj };
-    }, {});
-
-    return result;
-  };
-
-  return getRecursiveValue(tree);
-};
+export default convert;
