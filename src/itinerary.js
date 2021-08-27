4bed0dae309171077888dedca3357c9210a8046a
Ekaterina Ivanova <eivanova@cs-cart.com>
2021-08-27 16:46:51 +0400

    adds makeJoints

--- src/itinerary.js
+++ src/itinerary.js
@@ -1,43 +1,22 @@
 import _ from 'lodash';
 
+const makeJoints = (tree, parent) => {
+  const [leaf, children] = tree;
+  if (!Array.isArray(children)) {
+    return { [leaf]: [parent] };
+  }
+  const flatChildren = _.flatten(children);
+  const neighbours = _.concat(flatChildren.filter((child) => !Array.isArray(child)), parent);
+  return {
+    [leaf]: neighbours,
+    ...children.reduce((acc, child) => ({ ...acc, ...makeJoints(child, leaf) }), {}),
+  };
+};
+
 const itinerary = (tree, start, finish) => {
-  const flatTree = {};
-  const makeFlat = (node, parent) => {
-    const [name, branches] = node;
-    const children = [];
-    flatTree[name] = { parent, children };
-    if (Array.isArray(branches)) {
-      branches.forEach((branch) => {
-        const newNode = makeFlat(branch, name);
-        children.push(newNode);
-      });
-    }
-    return name;
-  };
-
-  makeFlat(tree, null);
-  const visited = [];
-
-  const findRoute = (city) => {
-    if (city === finish) {
-      return city;
-    }
-    let near = [...flatTree[city].children];
-    if (flatTree[city].parent !== null) {
-      near.push(flatTree[city].parent);
-    }
-
-    visited.push(city);
-    near = _.difference(near, visited);
-
-    if (near.length === 0) {
-      return [];
-    }
-    const route = near.flatMap(findRoute);
-    return route.length === 0 ? [] : [city, ...route];
-  };
-
-  return findRoute(start);
+  const joints = makeJoints(tree);
+  console.log(joints);
+  return joints;
 };
 
 export default itinerary;
